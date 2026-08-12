e94f30d897c655373d79113be65c0639e798bab0
lay2ss <lais222lai@gmail.com>
2026-08-11 22:32:56 -0300

    refactor(Socials.jsx): update UI

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,7 +1,7 @@
 const Socials = ({name, icon, link}) => {
   return (
     <a href={link}>
-        <div className="hover:text-[#9B53FB] border bg-white/10 rounded-xl px-4 py-3 orbitron cursor-pointer xs:min-w-60">
+        <div className="hover:text-[#9B53FB] transition duration-300 hover:translate-x-0.5 rounded-lg px-4 py-3 orbitron cursor-pointer xs:min-w-60 bg-[#9B53FB]/5">
             <div className="flex items-center gap-3">
                 <img className="w-6" src={icon} alt="social icon" />
                 <p className="text-lg">{name}</p>
